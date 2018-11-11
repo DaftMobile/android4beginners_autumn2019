08bc57cc79a0a48235877aab1ad5d8aaa0bb9f30
Konrad Kowalewski <konrad.kowalewski@daftcode.pl>
2018-11-11 14:23:16 +0100

    Materiały z komentarzami do zajęć nr 1

--- Android4Beginners1/src/android4beginners1/collections.kt
+++ Android4Beginners1/src/android4beginners1/collections.kt
@@ -2,9 +2,27 @@ package android4beginners1
 
 fun main(args: Array<String>) {
     // listy
+    val cities: MutableList<String> = mutableListOf("Warszawa", "Londyn", "Madryt")
+    cities.add("Rzym")
     // zbiory
+    val set = mutableSetOf("Red")
     // tablice
+    val array = arrayOf("Hallo")
     // tablice typów prostych
+    val intArray = intArrayOf(1)
     // mapy i pary
+    val map = mapOf(
+            Pair(1, "Jeden"),
+            Pair(2, "Dwa"),
+            3 to "Trzy"
+    )
+    println(cities)
     // pętle
+    for (city in cities) {
+        println(city)
+    }
+    // for with range
+    for (i in 10 until 1 step 2) {
+        println(i)
+    }
 }
--- Android4Beginners1/src/android4beginners1/person.kt
+++ Android4Beginners1/src/android4beginners1/person.kt
@@ -1,6 +1,40 @@
 package android4beginners1
 
+// główny konstruktor (primary constructor) z domyślnym parametrem
+class Person(val name: String, var age: Int, var job: Job? = null) {
+
+    fun birthday() {
+        age += 1
+    }
+
+    fun isOfAge() = age >= 18
+
+    fun timeTravel(years: Int) {
+        age += years
+    }
+
+    fun income() = job?.salary ?: 0
+}
+
+data class Job(val salary: Int)
+
+fun Person.isEmployed(): Boolean = job != null
 
 fun main(args: Array<String>) {
-    println("Hello, world! from Kotlin")
+    var a: Int = 8
+    var b: String = "Hallo"
+    var c: Boolean = true
+    b = "World"
+    val d = 10
+
+    val iosDev: Person = Person("Michał", 28, Job(1000))
+    val androidDev: Person = Person("Konrad", 26, Job(1000))
+
+    // sprawdzenie równości obiektów (Java .equals)
+    println(iosDev.job == androidDev.job)
+
+    // gettery i settery zamienione na property
+    val point = Point(5f, 5f)
+    point.x += 100f
+    println(point)
 }
