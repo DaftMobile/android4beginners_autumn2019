DaftMobile/android4beginners_autumn2018

Materiały do zajęć nr 3

Android4Beginners3/app/src/main/java/com/daftmobile/android4beginners3/MainActivity.kt
@@ -1,0 +1,31 @@
+package com.daftmobile.android4beginners3
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.Random
+
+class MainActivity : LifecycleLoggingActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showColorButton.setOnClickListener(this::showColorActivity)
+        inputButton.setOnClickListener(this::showInputActivity)
+    }
+
+    private fun showColorActivity(view: View) {
+        // tutaj otwórz ColorActivity
+    }
+
+    private fun showInputActivity(view: View) {
+        // tutaj otwórz InputActivity
+    }
+
+    private object RandomColor {
+        private val random = Random()
+
+        fun generate(): Int = Color.rgb(random.nextInt(255), random.nextInt(255), random.nextInt(255))
+    }
+}

DaftMobile/android4beginners_autumn2019

Android4Beginners3/app/src/main/java/com/daftmobile/android4beginners3/MainActivity.kt
@@ -1,0 +1,27 @@
+package com.daftmobile.android4beginners3
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : LifecycleLoggingActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showColorButton.setOnClickListener(this::showColorActivity)
+        inputButton.setOnClickListener(this::showInputActivity)
+    }
+
+    private fun showColorActivity(view: View) {
+        // tutaj otwórz ColorActivity
+    }
+
+    private fun showInputActivity(view: View) {
+        // tutaj otwórz InputActivity
+    }
+
+    fun generateRandomColor(): Int = Color.rgb(Random.nextInt(255), Random.nextInt(255), Random.nextInt(255))
+}