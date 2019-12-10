DaftMobile/android4beginners_autumn2018

Materiały do zajęć nr 5

Android4Beginners5/app/src/main/java/com/daftmobile/android4beginners5/vending/Bar.kt
@@ -1,0 +1,3 @@
+package com.daftmobile.android4beginners5.vending
+
+data class Bar(val name: String, val price: Int)

DaftMobile/android4beginners_autumn2019

Android4Beginners5/app/src/main/java/com/daftmobile/android4beginners5/sweets/Bar.kt
@@ -1,0 +1,3 @@
+package com.daftmobile.android4beginners5.sweets
+
+data class Bar(val name: String, val price: Int)